3801611203572b2ad6d0fd846d899c2ba88c49a2
fallenreaper <wllm.francis@gmail.com>
2019-01-02 21:15:28 -0500

    Max Value, though there is a risk of B/O

--- javascript/problems/smallest_difference.js
+++ javascript/problems/smallest_difference.js
@@ -24,6 +24,18 @@ function getSmallestDiff(arrA, arrB){
     return [sortedA[tuple[1]], sortedB[tuple[2]]]
 }
 
+//get Max with no if-else or comparison operators
+function getMax(a,b){
+    // THIS IS BROKEN.  a = -1 * MAX_VALUE  b = MAX_VALUE ==> bufferoverflow messing with Bits.
+    //we need to look at the sign of a-b
+    // return a-b
+    function flip (bit) {return 1^bit};
+    function sign(a) { return flip((a>>31)& 1)}
+
+    let k = sign(a-b); /// 1 or 0
+    let q = flip(k); /// opposite bit
+    return a*k + b*q;
+}
 function main(){
     let a = [1,3,15,11,2]
     let b = [23,127,235,19,8]
@@ -32,5 +44,8 @@ function main(){
     a = [6,7,8,9,10]
     b = [1,2,3,4,5]
     console.log(getSmallestDiff(a,b))
+
+
+    console.log(`Max Value: ${getMax(5, 10)}`);
 }
 main()
